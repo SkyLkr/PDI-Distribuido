db4f86c1f533d64272099ae1496dbe4886727fb2
SalesRyan <patricksales2010@hotmail.com>
2019-09-02 13:32:37 -0300

    Tela servidor

--- ApplicationServer/src/com/pdidist/appserver/App.java
+++ ApplicationServer/src/com/pdidist/appserver/App.java
@@ -1,4 +1,16 @@
 package com.pdidist.appserver;
 
+import javax.swing.*;
+
 public class App {
+    private JTextArea textArea1;
+    private JPanel mainPanel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setContentPane(new App().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
